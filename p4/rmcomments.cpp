24ea49bf4a4ce664ea30e9a182fd2d8a4fba5def
William E. Skeith <wes@cs.ccny.cuny.edu>
2022-03-23 19:31:33 -0400

    Project 4 skeleton + readme.

---
+++ p4/rmcomments.cpp
@@ -0,0 +1,25 @@
+/*
+ * CSc103 Project 4: uncommenting C/C++
+ * See readme.html for details.
+ * Please list all references you made use of in order to complete the
+ * assignment: your classmates, websites, etc.  Aside from the lecture notes
+ * and the book, please list everything.  And remember- citing a source does
+ * NOT mean it is okay to COPY THAT SOURCE.  What you submit here **MUST BE
+ * YOUR OWN WORK**.
+ * References:
+ *
+ *
+ * Finally, please indicate approximately how many hours you spent on this:
+ * #hours: 
+ */
+
+#include <cstdio> // printf and friends
+// strings might help if you want to do a nice job removing whitespace
+#include <string>
+using std::string;
+
+int main()
+{
+	/* TODO: write me... */
+	return 0;
+}
